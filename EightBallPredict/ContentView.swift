07175a59b33da388b6a93996b608078f4d9f41e6
SpiritAndPollen <jrtt212@gmail.com>
2025-07-20 07:17:15 +1000

    Prof Gallaughers Predict project

--- EightBallPredict/ContentView.swift
+++ EightBallPredict/ContentView.swift
@@ -6,14 +6,28 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var prediction = ""
     var body: some View {
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "record.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 350 , height: 350)
+                
+            Text(prediction)
+                .font(.largeTitle)
+                .frame(height: 120)
+            
+            Button("Predict") {
+            prediction = "Awesomness is on it's way"
+            }
+            buttonStyle(.borderedProminent)
+                .tint(.indigo)
+                .font(.title2)
+            
         }
         .padding()
     }
