512a1efce63dbed92c64ec17c0d954642fcf6d74
SpiritAndPollen <jrtt212@gmail.com>
2025-07-20 06:48:58 +1000

    Initial Commit

---
+++ EightBallPredict/EightBallPredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  EightBallPredictApp.swift
+//  EightBallPredict
+//
+//  Created by Jedda Tuuta on 20/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct EightBallPredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
