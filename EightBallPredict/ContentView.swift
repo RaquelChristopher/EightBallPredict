66c6013e088c090d300a029812aeb18bdb0eee2b
SpiritAndPollen <jrtt212@gmail.com>
2025-07-20 09:16:38 +1000

    finished first video of predict app

--- EightBallPredict/ContentView.swift
+++ EightBallPredict/ContentView.swift
@@ -8,6 +8,27 @@
 import SwiftUI
 struct ContentView: View {
     @State private var prediction = ""
+    let choicesArray = ["It is certain",
+                        "It is decidedly so",
+                        "Without a doubt",
+                        "Yes, definitely",
+                        "You may rely on it",
+                        "As I see it, yes",
+                        "Most likely",
+                        "Outlook good",
+                        "Yes",
+                        "Signs point to yes",
+                        "Reply hazy try again",
+                        "Ask again later",
+                        "Better not tell you now",
+                        "Cannot predict now",
+                        "Concentrate and ask again",
+                        "Don't count on it",
+                        "My reply is no",
+                        "My sources say no",
+                        "Outlook not so good",
+                        "Very doubtful"
+]
     var body: some View {
         
         
@@ -22,12 +43,14 @@ struct ContentView: View {
                 
             Text(prediction)
                 .font(.largeTitle)
+                .multilineTextAlignment(.center)
                 .frame(height: 120)
+                .animation(.default, value: prediction)
             
             Spacer()
                 
             Button("Predict") {
-                prediction = "Awesomness is on it's way!"
+                prediction = choicesArray.randomElement() ?? "empty array"
                 
             }
             .buttonStyle(.borderedProminent)
